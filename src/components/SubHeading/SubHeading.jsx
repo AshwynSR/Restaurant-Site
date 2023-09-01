ac75464b205ce3e0a8e38a785a375ee8361949da
Ashwin Rathore <ashwin.rathore2@wipro.com>
2023-09-01 12:50:33 +0530

    Started Gallery

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { images } from '../../constants';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+const SubHeading = (props) => (
+  <div style={{marginBottom: '0.5rem'}}>
+    <p className='p__cormorant'>{props.title}</p>
+    <img src={images.spoon} alt="spoon" className='spoon__img' />
   </div>
 );
 
--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className='app__wrapper_info'>
+      <SubHeading  title="Chef's Word" />
+      <h1 className='headtext__cormorant'>what We Believe In</h1>
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>Eat Fresh, Be Healthy</p>
+        </div>
+        <p className='p__opensans'>Hello there, this is the best restaurant</p>
+      </div>
+      <div className='app__chef-sign'>
+        <p>Kevin Lou</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="chef's sign" />
+      </div>
+    </div>
+
   </div>
 );
 
